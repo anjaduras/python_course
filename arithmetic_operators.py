40d85207d7cc9f7dffb4812c5bdde6fe7ebe5ab8
anjaduras <anna@duras.org>
2024-04-16 14:52:43 +0200

    coin calculator

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -12,3 +12,38 @@ final_price = original_price - discount_amount
 print(
     f"After a {discount_percent * 100:.0f}% discount, the final price is ${final_price:.2f}"
 )
+
+#Exercise 3: Coin Change Calculator
+amount = 175 
+euro_value = 100  # 1 euro in cents
+fifty_cent_value = 50
+twenty_cent_value = 20
+ten_cent_value = 10
+five_cent_value = 5
+one_cent_value = 1
+
+num_euros = amount // euro_value
+remaining_amount = amount % euro_value
+
+num_fifty_cent = remaining_amount // fifty_cent_value
+remaining_amount = remaining_amount % fifty_cent_value
+
+num_twenty_cent = remaining_amount // twenty_cent_value
+remaining_amount = remaining_amount % twenty_cent_value
+
+num_ten_cent = remaining_amount // ten_cent_value
+remaining_amount = remaining_amount % ten_cent_value
+
+num_five_cent = remaining_amount // five_cent_value
+remaining_amount = remaining_amount % five_cent_value
+
+num_one_cent = remaining_amount
+
+print(f"{amount} cents can be made up of:")
+print(f"{num_euros} euro coins")
+print(f"{num_fifty_cent} fifty cent coins")
+print(f"{num_twenty_cent} twenty cent coins")
+print(f"{num_ten_cent} ten cent coins")
+print(f"{num_five_cent} five cent coins")
+print(f"{num_one_cent} one cent coins")
+#to-do: try to connect this exersize with user input.
