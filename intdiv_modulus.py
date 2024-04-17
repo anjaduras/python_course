21c9e66824dc964f2f46463b5678c11c7380e283
anjaduras <anna@duras.org>
2024-04-17 11:31:21 +0200

    time converting

--- intdiv_modulus.py
+++ intdiv_modulus.py
@@ -1,4 +1,4 @@
-#Exercize 1: Leap Year Calculator
+# Exercize 1: Leap Year Calculator
 def is_leap_year(year):
     if year % 4 == 0:
         if year % 100 == 0:
@@ -14,3 +14,17 @@ if is_leap_year(year):
     print(f"{year} is a leap year")
 else:
     print(f"{year} is not a leap year")
+
+
+# Exercize 2: Converting Minutes to Hours
+def convert_minutes_to_hours(total_minutes):
+
+    hours = total_minutes // 60  # Integer division to get hours
+    minutes = total_minutes % 60  # Modulo to get remaining minutes
+    return f"{hours} hour{('s' if hours > 1 else '')} {minutes} minute{('s' if minutes > 1 else '')}"
+
+
+# Example usage
+total_minutes = 113
+time_str = convert_minutes_to_hours(total_minutes)
+print(f"{total_minutes} minutes is equal to {time_str}")
