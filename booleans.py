6d085eb75977a760e039602acdbd3698266400e6
anjaduras <anna@duras.org>
2024-04-18 11:11:55 +0200

    calendar transcription works

--- booleans.py
+++ booleans.py
@@ -44,9 +44,18 @@ def month_num_to_text(number):
     if 0 <= number <= 9:
         return month_transcription[number]
     else:
-        return "Choose a number must between 0 and 9)"
+        return "Choose a number between 1 and 12"
 
 
-number = 7
-text = number_to_text(number)
-print(f"{number} in text is: {text}")
+number = 2
+text = month_num_to_text(number)
+if number == 1:
+    print(f"{number}st month of the year is called {text}")
+elif number == 2:
+    print(f"{number}nd month of the year is called {text}")
+elif number == 3:
+    print(f"{number}rd month of the year is called {text}")
+elif 4 <= number <= 12:    
+    print(f"{number}th month of the year is called {text}")
+else:
+    print("Choose a number must between 1 and 12")
