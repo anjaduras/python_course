0f6278dc2419ccae564ca44bafe69e9710d312c4
anjaduras <anna@duras.org>
2024-04-22 09:17:59 +0200

    first user input task

--- user_input.py
+++ user_input.py
@@ -1,1 +1,11 @@
-#will focus on this soon. not yet though
+first_number = input("Enter the first number: ")
+second_number = input("Enter the second number: ")
+
+# This approach assumes valid input (numbers only)
+# If the user enters something else, the program will crash
+num1 = int(first_number)
+num2 = int(second_number)
+
+sum = num1 + num2
+
+print("The sum of", num1, "and", num2, "is", sum)
