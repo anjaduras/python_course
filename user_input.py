458e4557a551729e1508c7f5bd7441fd748447c5
anjaduras <anna@duras.org>
2024-04-22 09:48:03 +0200

    simple comp test draft

--- user_input.py
+++ user_input.py
@@ -17,3 +17,23 @@ end_message = "I'm also doing {} today, {}! Thanks for talking to me!".format(
     mood, name
 )
 print(end_message)
+
+"""Ex 3: Simplest Compatability Test"""
+
+number = int(input("Enter a number: ")) 
+
+user_q1 = input("Do you want to increase the number by 5 (yes/no)? ").lower()  # Get user choice in lowercase
+user_q2 = input("Do you want to increase the number by 5 (yes/no)? ").lower()
+user_q3 = input("Do you want to increase the number by 5 (yes/no)? ").lower()
+user_q4 = input("Do you want to increase the number by 5 (yes/no)? ").lower()
+
+if user_q1 == "yes":
+  number += 5  # Increase number by 5 if user says yes
+if user_q2 == "yes":
+  number += 5 
+if user_q3 == "yes":
+  number += 5 
+if user_q4 == "yes":
+  number += 5 
+
+print(number)
