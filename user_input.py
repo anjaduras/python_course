714072d9c13e42ca8a4f8fd516592cd8526fbaa2
anjaduras <anna@duras.org>
2024-04-22 10:05:43 +0200

    my first ever python quiz yayy

--- user_input.py
+++ user_input.py
@@ -20,20 +20,33 @@ print(end_message)
 
 """Ex 3: Simplest Compatability Test"""
 
-number = int(input("Enter a number: ")) 
+number = 0
+print("Welcome to my first Python Quizz! Let's check if we are compatible:)")
+user_q1 = input(
+    "Do you listen to music (yes/no)? "
+).lower()  # Get user choice in lowercase
+user_q2 = input("Do you like cats (yes/no)? ").lower()
+user_q3 = input("Do you drink coffee (yes/no)? ").lower()
+user_q4 = input("Do you have long-term goals of any kind (yes/no)? ").lower()
 
-user_q1 = input("Do you want to increase the number by 5 (yes/no)? ").lower()  # Get user choice in lowercase
-user_q2 = input("Do you want to increase the number by 5 (yes/no)? ").lower()
-user_q3 = input("Do you want to increase the number by 5 (yes/no)? ").lower()
-user_q4 = input("Do you want to increase the number by 5 (yes/no)? ").lower()
+number += 5 if user_q1 == "yes" else 0
+number += 5 if user_q2 == "yes" else 0
+number += 5 if user_q3 == "yes" else 0
+number += 5 if user_q4 == "yes" else 0
 
-if user_q1 == "yes":
-  number += 5  # Increase number by 5 if user says yes
-if user_q2 == "yes":
-  number += 5 
-if user_q3 == "yes":
-  number += 5 
-if user_q4 == "yes":
-  number += 5 
-
-print(number)
+if number <= 5:
+    print("Your score on my quiz is {}. We can't be friends :((".format(number))
+elif 10 <= number <= 15:
+    print("Your score on my quiz is {}. You're cool! Let's hang out.".format(number))
+elif number == 20:
+    print(
+        "Your score on my quiz is {}! Omg, are we soulmates? Because I sure think so!".format(
+            number
+        )
+    )
+else:
+    print(
+        "Your score is",
+        number,
+        ". This result is unexpected. Please check the quiz logic.",
+    )
