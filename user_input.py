11c3da7e9a1574d9895f0cff18788d2c1c01ac64
anjaduras <anna@duras.org>
2024-04-22 09:39:53 +0200

    user imput strings

--- user_input.py
+++ user_input.py
@@ -1,11 +1,19 @@
+"""Ex. 1: Collecting Numbers from User Input"""
+
 first_number = input("Enter the first number: ")
 second_number = input("Enter the second number: ")
-
 # This approach assumes valid input (numbers only)
 # If the user enters something else, the program will crash
 num1 = int(first_number)
 num2 = int(second_number)
-
 sum = num1 + num2
-
 print("The sum of", num1, "and", num2, "is", sum)
+
+"""Ex. 2: Collecting Strings from User Input"""
+
+name = input("Please enter your name: ")
+mood = input("How are you doing today, {}? ".format(name))
+end_message = "I'm also doing {} today, {}! Thanks for talking to me!".format(
+    mood, name
+)
+print(end_message)
