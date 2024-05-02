e52bc1e766a7c5476b025cc36f698d1993b9adc9
anjaduras <anna@duras.org>
2024-05-02 15:53:19 +0200

    basic dictionary operations

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+family = {
+    "Mom" : "Julia",
+    "Dad" : "Serhii",
+    "Daughter" : "Anna",
+    "Son" : "Jacob"
+}
+
+family["Daughter"] = "Anja"
+print(family)
+family["Pet"] = "Mira"
+print(family)
