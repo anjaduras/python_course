0ed92deb5c784fdb21d6ac95e4d3dbae9a035c08
anjaduras <anna@duras.org>
2024-05-21 12:33:38 +0200

    starting sets

---
+++ sets.py
@@ -0,0 +1,5 @@
+#Ways of defining sets
+'''set([1,2,3]) can be used as a difinition but 
+there is no built-in way to assign a name for it directly.
+The set() function simply returns a new set object based on the 
+provided iterable (like a list or another set).'''
