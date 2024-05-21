9f2b3f6307af11fdc9cecf50505ea7c2f85b8950
anjaduras <anna@duras.org>
2024-05-21 12:56:35 +0200

    basic sets operations

--- sets.py
+++ sets.py
@@ -1,11 +1,39 @@
-#1: Ways of defining sets
-'''set([1,2,3]) can be used as a difinition but 
+# 1: Ways of defining sets
+
+"""set([1,2,3]) can be used as a difinition but 
 there is no built-in way to assign a name for it directly.
 The set() function simply returns a new set object based on the 
-provided iterable (like a list or another set).'''
+provided iterable (like a list or another set)."""
 
-set1 = {1,2,3}
-l = [3,4,5]
+set1 = {1, 2, 3}
+l = [3, 4, 5]
 set2 = set(l)
-print(set1,set2)
+print(set1, set2)
 print(f"The length of the first set equals {len(set1)}")
+print()
+
+# 2: Disjointedness
+print(set1.isdisjoint(set2))
+# Prints False because these two sets have a joined element.
+print()
+
+# 3: Add, Remove and Discard
+set1.add(4)
+set1.add(1)
+print(set1)
+set1.remove(4)
+# set1.remove(100) returns a KeyError. However
+set1.discard(100)
+print(set1)
+print()
+
+# 4: Union VS Intersection
+print(set1 | set2)
+print(set1 & set2)
+print()
+
+# 5: Difference
+print(set1 - set2)
+print(set2 - set1)
+# SHowcase of set difference being uncommutative.
+print(set1 - set1)
