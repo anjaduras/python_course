c8eea0940ab2c48b0c80fe5b2b40e6ba12d16cd4
anjaduras <anna@duras.org>
2024-05-24 13:56:58 +0200

    morning commit

--- calendar_maker.py
+++ calendar_maker.py
@@ -1,1 +1,40 @@
-import datetime
+import time, sys, bext
+
+print('Press Ctrl-C to stop.')
+time.sleep(3)
+
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
+
+try:
+    while True: # Main program loop.
+        print(' ' * indent, end='')
+        bext.fg('red')
+        print('##', end ='')
+        bext.fg('yellow')
+        print('##', end='')
+        bext.fg('green')
+        print('##', end='')
+        bext.fg('blue')
+        print('##', end='')
+        bext.fg('cyan')
+        print('##', end='')
+        bext.fg('purple')
+        print('##')
+
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 1
+            if indent == 60: # (!) Change this to 10 or 30.
+                # Change direction:
+                indentIncreasing = False
+        
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 1
+            if indent == 0:
+                # Change direction:
+                indentIncreasing = True
+        time.sleep(0.02) # Add a slight pause.
+except KeyboardInterrupt:
+    sys.exit() # When Ctrl-C is pressed, end the program.
